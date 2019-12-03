a12a5b71fc2d15088a6aaba9aee20b33b9c0eaec
Otabek <otabek.akbaroff@gmail.com>
2019-12-03 16:39:29 -0500

    finished mvp

--- js/index.js
+++ js/index.js
@@ -38,5 +38,160 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
+
+/////////////////////////////////////////////NAVIGATIONS/////////////////////////////////////
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let nav=document.querySelector("nav");
+let navChildren=document.querySelectorAll("nav a");
+navChildren[0].textContent=siteContent.nav["nav-item-1"];
+navChildren[1].textContent=siteContent.nav["nav-item-2"];
+navChildren[2].textContent=siteContent.nav["nav-item-3"];
+navChildren[3].textContent=siteContent.nav["nav-item-4"];
+navChildren[4].textContent=siteContent.nav["nav-item-5"];
+let a=document.createElement("a");
+a.innerHTML="AppendChild";
+nav.appendChild(a);
+let a2=document.createElement("a");
+a2.textContent="PrependChild"
+nav.prepend(a2);
+
+for(let i=0; i<navChildren.length; i++){
+  navChildren[i].style.color="DarkGreen";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// document.body.style.backgroundColor='red';
+// let navChildren=document.querySelector("nav").children;
+// let counter=0;
+// Array.from(navChildren).forEach(function(item){
+//   let newArray=[];
+//   for(let x in siteContent.nav){
+//     if(siteContent.nav[x]==="img/logo.png"){
+//       break;
+//     }
+//     newArray.push(siteContent.nav[x]);
+//   }
+//   item.innerHTML=newArray[counter];
+//   item.style.color="#00ff00";
+//   counter++;
+// });
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+//////////////////////////////SECTION CLASS CTA///////////////////////////////////////////////
+let h1=document.querySelector("h1");
+// h1.innerHTML="DOM <br> IS <br> AWESOME";
+h1.innerHTML=siteContent.cta['h1'];
+
+let topImg=document.querySelector('.cta').lastElementChild;
+topImg.src=siteContent.cta['img-src'];
+
+let button=document.querySelector('button');
+button.innerHTML=siteContent.cta['button'];
+
+////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+///////////////////////////MAIN CONTENT///////////////////////////////////////////////////
+let features=document.querySelector(".top-content").firstElementChild.firstElementChild;
+let featureParagraph=document.querySelector(".top-content").firstElementChild.lastElementChild;
+
+features.innerHTML=siteContent["main-content"]['features-h4'];
+featureParagraph.innerHTML=siteContent["main-content"]['features-content'];
+
+let about=document.querySelector(".top-content").lastElementChild.firstElementChild;
+let aboutParagraph=document.querySelector(".top-content").lastElementChild.lastElementChild;
+
+about.innerHTML=siteContent["main-content"]['about-h4'];
+aboutParagraph.innerHTML=siteContent["main-content"]['about-content'];
+
+let midImage=document.getElementById("middle-img");
+midImage.src=siteContent["main-content"]['middle-img-src'];
+
+let services=document.querySelector('.bottom-content').firstElementChild.firstElementChild;
+let servicesParagraph=document.querySelector('.bottom-content').firstElementChild.lastElementChild;
+
+services.innerHTML=siteContent["main-content"]['services-h4'];
+servicesParagraph.innerHTML=siteContent["main-content"]['services-content'];
+
+
+
+let product=document.querySelector(".bottom-content").children[1].firstElementChild;
+let productParagraph=document.querySelector(".bottom-content").children[1].lastElementChild;
+
+product.innerHTML=siteContent["main-content"]['product-h4'];
+productParagraph.innerHTML=siteContent["main-content"]['product-content'];
+
+
+
+
+let vision=document.querySelector('.bottom-content').lastElementChild.firstElementChild;
+let visionParagraph=document.querySelector('.bottom-content').lastElementChild.lastElementChild;
+
+
+vision.innerHTML=siteContent["main-content"]['vision-h4'];
+visionParagraph.innerHTML=siteContent["main-content"]['vision-content'];
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////CONTACT////////////////////////////////////////////////
+
+let contact=document.querySelector('.contact').children[0];
+let paragraphOne=document.querySelector('.contact').children[1];
+let paragraphTwo=document.querySelector('.contact').children[2];
+let paragraphThree=document.querySelector('.contact').children[3];
+
+
+contact.innerHTML=siteContent["contact"]['contact-h4'];
+paragraphOne.innerHTML=siteContent["contact"]['address'];
+paragraphTwo.innerHTML=siteContent["contact"]['phone'];
+paragraphThree.innerHTML=siteContent["contact"]['email'];
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+///////////////////////////////////////////////FOOTER/////////////////////////////////////////////////////////////
+
+
+let footer=document.querySelector("footer");
+footer.innerHTML=siteContent["footer"]['copyright'];
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
